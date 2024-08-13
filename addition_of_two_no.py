7b8ab89384c3caffb6dcd06676d1746215e65a45
AtharvaBhosale15 <109413259+AtharvaBhosale15@users.noreply.github.com>
2024-08-13 19:47:56 +0530

    Add files via upload
    basic addition of two numbers program

---
+++ addition_of_two_no.py
@@ -0,0 +1,7 @@
+# take input for two numbers and covert the string to int type by int()
+a = int(input("enter first no :")) 
+b = int(input("enter second no :"))
+# print the addition of two numbers
+print("addition of two no is:-",a+b)
+
+
